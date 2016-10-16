fb555474be97e2ff0710ac0b3c17a23c14df92e7
KentWu <kent19871014@gmail.com>
2016-10-16 14:25:31 +0800

    21 練習 DB First 的開發流程，修改 Product 資料表的 ProductName 欄位型態與新增 IsDeleted 欄位

--- MVC5Course/Models/Product.cs
+++ MVC5Course/Models/Product.cs
@@ -11,8 +11,7 @@ namespace MVC5Course.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-
+    
     public partial class Product
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -23,10 +22,10 @@ namespace MVC5Course.Models
     
         public int ProductId { get; set; }
         public string ProductName { get; set; }
-        [Range(0, 10)]
         public Nullable<decimal> Price { get; set; }
         public Nullable<bool> Active { get; set; }
         public Nullable<decimal> Stock { get; set; }
+        public bool IsDeleted { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<OrderLine> OrderLine { get; set; }
