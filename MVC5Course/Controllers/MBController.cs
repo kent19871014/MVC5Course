19bf73693fcd8b60e101cd0150bfb76a57c730f8
KentWu <kent19871014@gmail.com>
2016-10-29 14:34:08 +0800

    42 練習從 Action 傳資料到 View 裡 (ViewData, ViewBag, TempData)

---
+++ MVC5Course/Controllers/MBController.cs
@@ -0,0 +1,49 @@
+﻿using MVC5Course.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class MBController : Controller
+    {
+        // GET: MB
+        public ActionResult Index()
+        {
+            ViewData["temp1"] = "Hello";
+
+            var b = new ClientLoginViewModels()
+            {
+                FirstName = "Kent",
+                LastName = "Wu"
+            };
+            
+            ViewData["Temp2"] = b;
+
+            return View();
+        }
+
+        public ActionResult MyForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult MyForm(ClientLoginViewModels c)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["MyFormData"] = c;
+                return RedirectToAction("MyFormResult");
+            }
+            return View();
+        }
+
+        public ActionResult MyFormResult()
+        {
+            return View();
+        }
+    }
+}
