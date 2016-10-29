c2edd46fdb68a5080ac2b6b24b3cc1682b141642
KentWu <kent19871014@gmail.com>
2016-10-29 13:23:45 +0800

    40 練習 FileResult 的使用，顯示或下載一個圖檔

--- MVC5Course/Controllers/ARController.cs
+++ MVC5Course/Controllers/ARController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,6 +13,23 @@ namespace MVC5Course.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult ContentTest()
+        {
+            return Content("開發實戰", "text/plan", Encoding.GetEncoding("Big5"));
+        }
+
+        public ActionResult FileTest()
+        {
+            var filePath = Server.MapPath("~/Content/imgres.jpg");
+            return File(filePath, "image/jpeg");
+        }
+        public ActionResult FileTest2()
+        {
+            var filePath = Server.MapPath("~/Content/imgres.jpg");
+
+            return File(filePath, "image/jpeg", "PPAP.jpg");
         }
     }
 }
